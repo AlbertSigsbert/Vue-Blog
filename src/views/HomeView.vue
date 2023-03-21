2c3da034a062a66b94b9b4476cceaaee22779d44
Albert <ishengomaalbert@gmail.com>
2023-03-21 11:17:22 +0300

    Refs vs Reactive

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,32 +1,40 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <p ref="p">My name is {{ name }} and my age is {{age}}</p>
+    <h2>Refs</h2>
+    <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
     <button @click="handleClick">Click Me</button>
     <button @click="age++">Add Age By 1</button>
-    <input type="text" v-model="name">
+    <input type="text" v-model="name" />
+
+    <h2>Reactive</h2>
+    <p>Ninja one is {{ninjaOne.name}} and is {{ninjaOne.age}} years old</p>
+    <button @click="updateNinja">Update Ninja</button>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, reactive } from "vue";
 
 export default {
-  name: 'HomeView',
-  setup(){
-    let name = ref('John')
-    let age = ref(25)
+  name: "HomeView",
+  setup() {
+    let name = ref("John");
+    let age = ref(25);
 
-   const p = ref(null);
-
+    const ninjaOne = reactive({ name: "Shinsuke", age: 25 });
+    const p = ref(null);
 
     const handleClick = () => {
       // p.value.textContent = "Hello,World";
-      name.value = "Mario"
-    }
+      name.value = "Mario";
+    };
+    const updateNinja = () => {
+      // p.value.textContent = "Hello,World";
+      ninjaOne.name = "Matsura";
+    };
 
-    return {name,age, handleClick, p}
-  }
- 
-}
+    return { name, age, handleClick, p, updateNinja, ninjaOne };
+  },
+};
 </script>
